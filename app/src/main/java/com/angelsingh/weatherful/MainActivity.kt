adf93993902265bbdb5c07e540a134d1b033a577
aenxso <angel8_singh8@hotmail.com>
2020-12-08 04:25:07 -0800

    Change button effect on click Hide keyboard on outside click Add functionality to softkeyboard search key

--- app/src/main/java/com/angelsingh/weatherful/MainActivity.kt
+++ app/src/main/java/com/angelsingh/weatherful/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.angelsingh.weatherful
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.angelsingh.weatherful.api.ApiRequests
 import com.angelsingh.weatherful.model.WeatherData
@@ -32,6 +37,9 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                 fetchWeather(searchField.query.toString())
             }
         }
+
+        hideKeyboard(contentMain)
+        searchPressed((searchField))
     }
 
     private fun fetchWeather(searchString: String) {
@@ -45,8 +53,10 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
                     val weatherData = response.body()!!
                     Log.d("TAG", weatherData.cityName)
-                    degressNumLabel.text = weatherData.main.temperature.toString()
+                    degressNumLabel.text = String.format("%.1f", weatherData.main.temperature)
                     cityLabel.text = weatherData.cityName
+
+                    searchField.setQuery("", false)
 
                 }
 
@@ -56,4 +66,31 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             })
         }
     }
+
+    private fun hideKeyboard(view: View) {
+        view.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+                return true
+            }
+        })
+    }
+
+    private fun searchPressed(search: SearchView) {
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (searchField.query.toString().isNotEmpty()) {
+                    fetchWeather(searchField.query.toString())
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.i("TAG","Llego al querytextchange")
+                return true
+            }
+        })
+    }
+
 }
